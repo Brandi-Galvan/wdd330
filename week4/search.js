4454a273730a79471bef53c01dd85f015e403131
Brandi Galvan <97068729+Brandi-Galvan@users.noreply.github.com>
2022-05-14 13:49:25 -0700

    My brain hurts from reading

--- week4/search.js
+++ week4/search.js
@@ -1,4 +1,5 @@
-const form = document.forms['search'];
+// JavaScript for the Searching Example
+/* const form = document.forms['search'];
 
 const input = form.elements.searchInput;
 
@@ -10,6 +11,37 @@ const input = form.elements.searchInput;
 
 form.addEventListener ('submit', search, false);
 
-function search() {
-    alert(' Form Submitted');
+function search(event) {
+    alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
+}
+
+input.value = 'Search Here';
+
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
+    }
+}, false);
+
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false); */
+
+// JavaScript for the Hero Project
+
+const form = document.forms['hero'];
+form.addEventListener('submit', makeHero, false);
+
+function makeHero(event) {
+
+    event.preventDefault(); // prevent the form from being submitted
+
+    const hero = {}; // create an empty object
+
+    hero.name = form.heroName.value; // create a name property based on the input field's value
+
+    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
+    return hero;
 }
